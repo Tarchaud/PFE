72d383b7c7e8b27a977c94dec78bbf19f5b6a100
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-02-02 11:44:48 +0100

    add sharedModule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 import { MenuComponent } from './template/menu/menu.component';
 import { FooterComponent } from './template/footer/footer.component';
 
@@ -13,6 +14,7 @@ import { FooterComponent } from './template/footer/footer.component';
     FooterComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ front/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    FormsModule,
+    HttpClientModule,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+  ]
+})
+export class SharedModule { }
