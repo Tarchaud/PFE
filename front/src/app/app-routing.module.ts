b96802b1df91ee2c411995297a9664ab40e8b3a8
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-02-02 17:59:50 +0100

    add routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { WikiItemsComponent } from './pages/wiki-items/wiki-items.component';
+
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'wiki-items', component: WikiItemsComponent },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: '**', redirectTo: '/accueil' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
