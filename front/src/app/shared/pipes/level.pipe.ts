3a70f37e7cb077f58a8dc4f7f3abf2c32fb60ea6
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-02-05 16:50:06 +0100

    pages : wiki-items

--- front/src/app/shared/pipes/level.pipe.ts
+++ front/src/app/shared/pipes/level.pipe.ts
@@ -7,10 +7,6 @@ import { ItemI } from '../models/item-i';
 export class LevelPipe implements PipeTransform {
 
   transform(items: ItemI[], levelMin: number, levelMax: number): ItemI[] {
-    console.log(levelMax);
-    console.log(items.length);
-
-
     return items.filter(i => i.definition.item.level >= levelMin && i.definition.item.level <= levelMax);
   }
 
--- front/src/app/shared/services/equipement-item-type.service.ts
+++ front/src/app/shared/services/equipement-item-type.service.ts
@@ -10,15 +10,17 @@ import { EquipmentItemTypeI } from '../models/equipment-item-type-i';
 export class EquipementItemTypeService {
   public arr_equipementItemTypes : EquipmentItemTypeI[] = [];
   public map_equipementItemTypes = new Map<Number, EquipmentItemTypeI>();
+  public map_equipementParentItemTypes = new Map<number, number[]>();
 
   constructor(private http : HttpClient) { }
 
   //TODO : link to the backend
   getAllEquipementItemTypes() {
-    return this.http.get<EquipmentItemTypeI[]>('assets/data/equipementItemTypes.json').subscribe({
+    return this.http.get<EquipmentItemTypeI[]>('assets/data/equipmentItemTypes.json').subscribe({
       next : (data : EquipmentItemTypeI[]) => {
         this.arr_equipementItemTypes = data;
         this.mapEquipementItemTypes();
+        this.mapEquipementParentItemTypes();
       },
       error : (err) => {
         console.log(err);
@@ -32,6 +34,24 @@ export class EquipementItemTypeService {
   mapEquipementItemTypes() {
     this.arr_equipementItemTypes.forEach((equipementItemType) => {
       this.map_equipementItemTypes.set(equipementItemType.definition.id, equipementItemType);
+    });
+  }
+
+  mapEquipementParentItemTypes() {
+    this.arr_equipementItemTypes.forEach((equipementItemType) => {
+      if(equipementItemType.definition.parentId !== undefined ) {
+        if(this.map_equipementParentItemTypes.has(equipementItemType.definition.parentId)) {
+        this.map_equipementParentItemTypes.get(equipementItemType.definition.parentId)?.push(equipementItemType.definition.id);
+        } else if(this.map_equipementItemTypes.has(equipementItemType.definition.parentId)){
+          this.map_equipementParentItemTypes.set(equipementItemType.definition.parentId, [equipementItemType.definition.id]);
+        } else if(!this.map_equipementParentItemTypes.has(equipementItemType.definition.id)) {
+          this.map_equipementParentItemTypes.set(equipementItemType.definition.id, [equipementItemType.definition.id]);
+        }
+      }
+      this.map_equipementParentItemTypes.delete(647);
+      this.map_equipementParentItemTypes.delete(537);
+      this.map_equipementParentItemTypes.delete(480);
+
     });
   }
 }
